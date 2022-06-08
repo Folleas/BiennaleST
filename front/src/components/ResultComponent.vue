aa9fb9d0fcb93b6c4d172340532411b7f40eece3
Folleas <rayann.folleas@epitech.eu>
2022-06-08 12:43:46 +0200

    [MOD] : correct back url

--- front/src/components/ResultComponent.vue
+++ front/src/components/ResultComponent.vue
@@ -7,7 +7,7 @@ export default {
         }
     },
     created(){
-        axios.get("https://collectifae:5000/api/forms")
+        axios.get("https://collectifae.com:5000/api/forms")
         .then(response => this.results = response.data);
     }
 }
