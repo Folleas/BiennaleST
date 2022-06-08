92ee8e9d7d9ebcaa127350c2658dded08d3f3bd2
Folleas <rayann.folleas@epitech.eu>
2022-06-08 12:39:51 +0200

    [MOD] : cors

--- back/server.js
+++ back/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8080"
+  origin: "https://collectifae.com/"
 };
 
 app.use(cors(corsOptions));
