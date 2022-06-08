d9fab379719ccdfd0a01c556f3c852fd16cd7be1
Folleas <rayann.folleas@epitech.eu>
2022-06-08 13:05:10 +0200

    [MOD] : api url to node

--- back/server.js
+++ back/server.js
@@ -32,7 +32,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: " " });
 });
 app.post("/", (req, res) => {
   res.json({ message: req.body });
